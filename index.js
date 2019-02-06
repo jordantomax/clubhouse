4d40c0fb943e8291db2493c1e0f55fe6ce6e5bcd
Jordan Cooperman <jordancooperman@gmail.com>
2019-02-06 18:17:44 -0500

    Finish pulling values from clubhouse and sending to Google Sheets, just want to extend the forumula on the next few columns to complete the work

--- index.js
+++ index.js
@@ -3,37 +3,120 @@
 const clubhouse = require('commander')
 const fetch = require('node-fetch')
 const { version } = require('./package.json')
+const authorizeGoogle = require('./google-auth')
+const { google } = require('googleapis')
+const readline = require('readline')
 
-async function getStories (days) {
-  console.log('your looking for stories from ' + days + ' ago.')
-  const token = process.env.CLUBHOUSE_API_TOKEN
-  if (!token) return console.log('CLUBHOUSE_API_TOKEN environment variable is not set')
-  const url = 'https://api.clubhouse.io/api/v2/'
+const API_HOST = 'https://api.clubhouse.io'
+const TOKEN = process.env.CLUBHOUSE_API_TOKEN
+const SPREADSHEET_ID = '1Zsyutqnb-uYZHPXt3QNninmsHPxODhaPEd16XLHTgj4'
+const sheets = google.sheets({ version: 'v4' })
 
-  let query = `teams?token=${token}`
+const userInput = readline.createInterface(process.stdin, process.stdout)
+
+function getFormattedDate (date) {
+  const year = date.getFullYear()
+  const month = ('0' + (date.getMonth() + 1)).slice(-2)
+  const day = ('0' + date.getDate()).slice(-2)
+  return `${year}-${month}-${day}`
+}
+
+function question (text) {
+  return new Promise((resolve, reject) => {
+    userInput.question(text, answer => {
+      resolve(answer)
+    })
+  })
+}
+
+async function pushPointsToGoogleSheet (end, pointsByTeam) {
+  for (const team in pointsByTeam) {
+    const numberOfMembers = await question(`How many team members for ${team}? `)
+    const { feature, chore, bug } = pointsByTeam[team]
+    authorizeGoogle(auth => {
+      const request = {
+        auth,
+        spreadsheetId: SPREADSHEET_ID,
+        valueInputOption: 'USER_ENTERED',
+        insertDataOption: 'INSERT_ROWS',
+        range: `${team}!A:D`,
+        resource: {
+          majorDimension: 'ROWS',
+          values: [
+            ['CLI', end, feature, chore, bug, numberOfMembers]
+          ]
+        }
+      }
+
+      sheets.spreadsheets.values.append(request, (err, res) => {
+        if (err) return console.log('The API returned an error: ' + err)
+      })
+    })
+  }
+  console.log('Points successfully sent to Google Sheets :)')
+  userInput.close()
+}
+
+async function search (path, stories) {
+  const { next, data } = await fetch(`${API_HOST}${path}&token=${TOKEN}`).then(res => res.json())
+  let newStories = stories.slice().concat(data)
+  if (next) newStories = search(next, newStories)
+  return newStories
+}
+
+async function listPointsByTeam (numberOfDays, endDate = new Date()) {
+  const startDate = new Date()
+  startDate.setDate(endDate.getDate() - numberOfDays)
+  const start = getFormattedDate(startDate)
+  const end = getFormattedDate(endDate)
+  console.log(`Finding point values from ${start} to ${end}`)
+  if (!TOKEN) return console.log('CLUBHOUSE_API_TOKEN environment variable is not set')
 
   console.log('fetching teams...')
-  const teams = await fetch(url + query).then(res => res.json())
-  query = `search/stories?token=${token}&query=completed:2018-12-09..2018-12-16&page_size=25`
+  const teams = await fetch(`${API_HOST}/api/v2/teams?token=${TOKEN}`).then(res => res.json())
 
   console.log('fetching stories...')
-  // THIS ONLY CONTAINS 25 stories, need access to all of the stories
-  const stories = await fetch(url + query).then(res => res.json())
+  const path = '/api/v2/search/stories'
+  const query = `?query=completed:${start}..${end}&page_size=25`
+  const stories = await search(`${path}${query}`, [])
+
+  const storiesByTeam = {}
+  const pointsByTeam = {}
   for (let i = 0; i < teams.length; i++) {
-    const teamStories = stories.data.filter(story => {
+    const team = teams[i].name
+
+    storiesByTeam[team] = {
+      feature: [],
+      chore: [],
+      bug: []
+    }
+
+    pointsByTeam[team] = {
+      feature: 0,
+      chore: 0,
+      bug: 0
+    }
+
+    stories.filter(story => {
       return teams[i].project_ids.includes(story.project_id)
+    }).forEach(story => {
+      storiesByTeam[team][story.story_type] = story
+      pointsByTeam[team][story.story_type] += story.estimate || 0
     })
-    console.log(teams[i].name, teamStories.map(story => story.id))
   }
+
+  console.log('Pushing data to Google Sheets...')
+  console.log(pointsByTeam)
+  pushPointsToGoogleSheet(end, pointsByTeam)
 }
 
 clubhouse
   .version(version)
 
 clubhouse
-  .command('stories <days>')
+  .command('list-points <number-of-days> [end-date]')
   .description('get story values for a specified number of days')
-  .action(getStories)
+  .action(listPointsByTeam)
 
 clubhouse
   .command('*')
